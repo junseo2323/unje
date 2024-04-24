b9a051824c9ef09ed76d2ca685d220f069dbaf9b
junseo23 <hunseol03@gmail.com>
2024-04-24 18:39:59 +0900

    routing test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    //redirects를 적용
+    async redirects() {
+        return [
+          {
+            source: '/',
+            destination: '/main',
+            permanent: true, //캐쉬여부로 true를 하면 캐쉬에서 저장해서 리다이랙트 진행
+          },
+        ]
+      },
+  }
+  
 export default nextConfig;
